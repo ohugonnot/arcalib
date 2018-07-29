d7f010837069891d04be1d721a3b585e097332e6
ohugonnot <folken70@hotmail.com>
2018-07-29 17:33:31 +0200

    Modifiction du tri par nombre d'inclusion

--- src/AppBundle/Repository/EssaisRepository.php
+++ src/AppBundle/Repository/EssaisRepository.php
@@ -34,9 +34,11 @@ class EssaisRepository extends \Doctrine\ORM\EntityRepository
     public function getQuery(User $user, $search)
     {
         $queryBuilder = $this->createQueryBuilder('e')
+            ->select('COUNT(DISTINCT i) AS  nb', 'e')
             ->leftJoin('e.inclusions', 'i')
             ->addSelect("i")
             ->where("e.nom like :search or e.numeroInterne like :search or e.statut like :search or e.typeEssai like :search or e.typeProm like :search or e.dateOuv like :search or e.dateClose like :search")
+            ->groupBy("e.id")
             ->setParameter('search', '%' . $search . '%');
 
         $queryBuilder = $this->joinUserWhereUser($queryBuilder, $user);
