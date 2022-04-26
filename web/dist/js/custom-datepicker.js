6ccb03f25e6a6feee7c5b24b4dfa5db1e1e53864
Laurent Partal <contact@reunisoft.re>
2022-04-26 17:04:30 +0400

    changement visite edition

--- web/dist/js/custom-datepicker.js
+++ web/dist/js/custom-datepicker.js
@@ -5,4 +5,5 @@ $('#arcoffice-add-visite').on('shown.bs.modal', function () {
         var dtEnd = moment(dtStart, "DD/MM/YYYY hh:mm").add(1, 'hours');
         dtPickerEndVisite.val(dtEnd.format("DD/MM/YYYY hh:mm"));
     });
+
 })
