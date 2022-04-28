5b3a19d61a768bfa8ef5428277bb3db3fdf59de6
odilon <odilon@goin-invest.com>
2022-04-28 12:31:11 +0200

    Maj agenda

--- web/dist/js/custom-datepicker.js
+++
@@ -1,9 +0,0 @@
-$('#arcoffice-add-visite').on('shown.bs.modal', function () {
-    $('#dtStartVisite').datetimepicker().on("dp.change",function() {
-        var dtPickerEndVisite = $('#dtEndVisite');
-        var dtStart = $('#dtStartVisite').val();
-        var dtEnd = moment(dtStart, "DD/MM/YYYY hh:mm").add(1, 'hours');
-        dtPickerEndVisite.val(dtEnd.format("DD/MM/YYYY hh:mm"));
-    });
-
-})
