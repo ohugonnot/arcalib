58cc047184dc2af4683d723f70b2f0bcb8508749
odilon <odilon@goin-invest.com>
2022-05-05 23:34:00 +0200

    style de benÃ

--- web/dev/js/custom/patient.js
+++ web/dev/js/custom/patient.js
@@ -1,6 +1,20 @@
 if ($("#patient").length > 0) {
 
     Vue.component('date-picker', VueBootstrapDatetimePicker);
+    $.extend(true, $.fn.datetimepicker.defaults, {
+        icons: {
+            time: 'far fa-clock',
+            date: 'far fa-calendar',
+            up: 'fas fa-arrow-up',
+            down: 'fas fa-arrow-down',
+            previous: 'fas fa-chevron-left',
+            next: 'fas fa-chevron-right',
+            today: 'fas fa-calendar-check',
+            clear: 'far fa-trash-alt',
+            close: 'far fa-times-circle'
+        }
+    });
+
     const patient = new Vue({
 
         delimiters: ['[[', ']]'],
