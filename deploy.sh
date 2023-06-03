48f573da98deefcf910ca471e36d41f821dca057
asfolken <folken70@hotmail.com>
2023-06-03 21:45:38 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-git pull
+sed -ri "s/version:.*/version: $(date '+%s')/g" ./app/config/parameters.yml
+cat ./app/config/parameters.yml | grep "version"
 php bin/console cache:clear
 php bin/console cache:clear --env=prod
 php bin/console fos:js-routing:dump
-php bin/console fos:js-routing:dump -eprod
-sed -ri "s/version:.*/version: $(date '+%s')/g" ./app/config/parameters.yml
-cat ./app/config/parameters.yml | grep "version"
+php bin/console fos:js-routing:dump --env=prod
 cd web
 gulp build
 cd ./..
-chown -R arcalib:arcalib /home/arcalib/
+chown -R arcalib:arcalib ./
 chmod -R 777 ./var/
+chown -R www-data ./var/
