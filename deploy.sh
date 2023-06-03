4b203ba18b52f6af35b7e9c31aaa10394077bcfe
root <root@srv-web-rc.infirmerie-protestante.com>
2023-06-03 21:19:08 +0200

    ajout de la modif version dans le deploy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,11 @@ git pull
 php bin/console cache:clear
 php bin/console cache:clear --env=prod
 php bin/console fos:js-routing:dump
-php bin/console fos:js-routing:dump --env=prod
-chmod -R 777 ./var/cache
+php bin/console fos:js-routing:dump -eprod
+sed -ri "s/version:.*/version: $(date '+%s')/g" ./app/config/parameters.yml
+cat ./app/config/parameters.yml | grep "version"
 cd web
 gulp build
 cd ./..
+chown -R arcalib:arcalib /home/arcalib/
+chmod -R 777 ./var/
